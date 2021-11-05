866ce5c9a3640568924f04e949bcff0261edfc5d
Yaakov <dbrionesmtz@yahoo.com>
2021-11-04 20:17:13 -0600

    configuracion inicial del layout y del index

---
+++ resources/views/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.web')
+
+@section('content')
+<div class="text-center">
+    <h1 class="text-3xl text-gray-700 mb-2 uppercase">Ultimos Cursos</h1>
+    <h2 class="text-xl text-gray-600">Formate online como pro en tecnología</h2>
+    <h3 class="text-lg text-gray-600">El 70% porciento duplica sus ingresos</h3>
+</div>
+@endsection

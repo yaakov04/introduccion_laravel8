f169a40dd2c21c02aa43c44beb3f0acb13d140a3
yaakov04 <dbrionesmtz@yahoo.com>
2021-11-05 12:57:15 -0600

    creando la pagina de cursos

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PageController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,7 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::view('/', 'index')->name('home');
+Route::get('/', [PageController::class, 'home'])->name('home');
+Route::get('course/{course:slug}', [PageController::class, 'course'])->name('course');
+
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');

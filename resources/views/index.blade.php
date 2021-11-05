68607e7031f7149f580af5eaa3f13f55527e315d
Yaakov <dbrionesmtz@yahoo.com>
2021-11-04 20:39:34 -0600

    configurando un componente en livewire

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,4 +6,5 @@
     <h2 class="text-xl text-gray-600">Formate online como pro en tecnología</h2>
     <h3 class="text-lg text-gray-600">El 70% porciento duplica sus ingresos</h3>
 </div>
+<livewire:course-list>
 @endsection
